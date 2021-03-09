be99ff4f72b707f875ffb9fbca13001daff6fc51
wst24365888 <hansen0715.2@gmail.com>
2021-03-09 19:51:45 +0800

    v1 stable

--- battle_team_constructor.py
+++ battle_team_constructor.py
@@ -17,10 +17,11 @@ for i, battleRateRowI in enumerate(data[1:]):
         for k, battleRateRowK in enumerate(data[1:]):
             if(j>=k):
                 continue
-            
+
             teamCoverage = np.max(np.array([battleRateRowI, battleRateRowJ, battleRateRowK]), axis=0)[1:]
             teamSafety = (battleRateRowI[1:] + battleRateRowJ[1:] + battleRateRowK[1:])/3
             teamCounters = battleRateRowI[1:] + battleRateRowJ[1:] + battleRateRowK[1:]
+            
             if (data[i+1][0] + data[j+1][0] + data[k+1][0] >= OWN_POKEMON) and (all(e >= COVERAGE for e in teamCoverage) and (all(e >= SAFETY for e in teamSafety) and (np.average(teamCoverage, weights=data[0][1:]) > COVERAGE_AVG))):
                 print("1. {}\n2. {}\n3. {}\n".format(reader.values[i+2][0], reader.values[j+2][0], reader.values[k+2][0]))
                 print("Coverage Avg: {}\nSafety Avg: {}\n".format(np.average(teamCoverage, weights=data[0][1:]), np.average(teamSafety, weights=data[0][1:])))
