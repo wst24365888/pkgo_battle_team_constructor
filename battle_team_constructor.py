c0c7abb90f9e446988199637235c01afdfd13fff
wst24365888 <hansen0715.2@gmail.com>
2021-03-09 19:48:24 +0800

    change display

--- battle_team_constructor.py
+++ battle_team_constructor.py
@@ -22,8 +22,5 @@ for i, battleRateRowI in enumerate(data[1:]):
             teamCounters = battleRateRowI[1:] + battleRateRowJ[1:] + battleRateRowK[1:]
             if (data[i+1][0] + data[j+1][0] + data[k+1][0] >= OWN_POKEMON) and (all(e >= COVERAGE for e in teamCoverage) and (all(e >= SAFETY for e in teamSafety) and (np.average(teamCoverage, weights=data[0][1:]) > COVERAGE_AVG))):
                 print("1. {}\n2. {}\n3. {}\n".format(reader.values[i+2][0], reader.values[j+2][0], reader.values[k+2][0]))
-                print('''
-                Coverage Avg: {}
-                Safety Avg: {}
-                '''.format(np.average(teamCoverage, weights=data[0][1:]), np.average(teamSafety, weights=data[0][1:])))
+                print("Coverage Avg: {}\nSafety Avg: {}\n".format(np.average(teamCoverage, weights=data[0][1:]), np.average(teamSafety, weights=data[0][1:])))
                 print("Weakest: {} {}\nCounter: {} {}\n".format(reader.values[0][np.argmin(teamCoverage)+2], np.min(teamCoverage), reader.values[0][np.argmin(teamCounters)+2], np.min(teamCounters)/3))
